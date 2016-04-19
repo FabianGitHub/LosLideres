1b4ba569008234888e650141da24f57553cea14a
FabianGitHub <palomo22009@gmail.com>
2016-04-18 21:01:59 -0500

    Yo hice esto

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+ <head>
+  <title>Prueba de PHP</title>
+ </head>
+ <body>
+ <?php echo '<p>Hola Mundo</p>'; 
+ ##hola compañeros estes es mi comentario en php by FABIAN xD
+ ?>
+ </body>
+</html>
